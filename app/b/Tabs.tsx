3874f9df93a792b6587135bb0f032ba4f3cb66e1
Dilshod <deebov@yandex.com>
2025-12-16 13:52:08 +0100

    Replace tabs with pure react aria tabs

--- app/b/Tabs.tsx
+++ app/b/Tabs.tsx
@@ -1,19 +1,26 @@
 "use client";
 
-import { Tabs } from "@/components/application/tabs/tabs";
 import { usePathname } from "next/navigation";
-
-const tabs = [
-  { id: "tab-0", label: "Tab 0" },
-  { id: "tab-1", label: "Tab 1" },
-];
+import { Tab, TabList, Tabs } from "react-aria-components";
 
 export default function PageTabs() {
   usePathname();
 
   return (
     <Tabs className="mt-4">
-      <Tabs.List items={tabs} />
+      <TabList
+        className="flex gap-2"
+        items={[
+          { id: "tab-0", label: "Tab 0" },
+          { id: "tab-1", label: "Tab 1" },
+        ]}
+      >
+        {(item) => (
+          <Tab key={item.id} id={item.id}>
+            {item.label}
+          </Tab>
+        )}
+      </TabList>
     </Tabs>
-    );
-  }
+  );
+}
